5c4e10ee4836f6c0f249bd74082cf1a3744f4c91
Mabizza <william.paulsen@umuzi.org>
2022-03-08 12:35:56 +0200

    Prettied up a Task06 with Prettier

--- Task06.js
+++ Task06.js
@@ -1,14 +1,11 @@
-function maximum(...numbers)
-{
-    maxno = numbers[0];
+function maximum(...numbers) {
+	let maxno = numbers[0];
 
-    for (let i=0; i < numbers.length; i++)
-    {
-        if (numbers[i] > maxno)
-            maxno = numbers[i];
-    }
+	for (let i = 0; i < numbers.length; i++) {
+		if (numbers[i] > maxno) maxno = numbers[i];
+	}
 
-   console.log(maxno);
+	console.log(maxno);
 }
-//---------------------------------------------
-maximum(15, 7, 10);				// Task 0.6
+
+maximum(15, 7, 10); // Task 0.6
