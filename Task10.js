3505dc5751aedcfd958c550de0d8d737af05ed10
Mabizza <william.paulsen@umuzi.org>
2022-03-08 12:36:10 +0200

    Prettied up a Task10 with Prettier

--- Task10.js
+++ Task10.js
@@ -1,10 +1,9 @@
-function intersect(a, b) 
-{
-    let setA = new Set(a);
-    let setB = new Set(b);
-    let intersection = new Set([...setA].filter(x => setB.has(x)));
-	let common = Array.from(intersection); 
-	console.log("Common letters: " + common.join() ); 
+function intersect(a, b) {
+	let setA = new Set(a);
+	let setB = new Set(b);
+	let intersection = new Set([...setA].filter((x) => setB.has(x)));
+	let common = Array.from(intersection);
+	console.log("Common letters: " + common.join());
 }
-//---------------------------------------------
+
 intersect("house", "computers");
