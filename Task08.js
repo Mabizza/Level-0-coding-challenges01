b3bb84c7ce69273c8112c73e7abb97254f1eab17
Mabizza <william.paulsen@umuzi.org>
2022-03-08 12:36:06 +0200

    Prettied up a Task08 with Prettier

--- Task08.js
+++ Task08.js
@@ -1,29 +1,22 @@
-function format (fmtstr) 
-{
-    let args = Array.prototype.slice.call(arguments, 1);
-    return fmtstr.replace(/\{(\d+)\}/g, function (match, index) 
-    {
-        return args[index];
-    });
+function format(fmtstr) {
+	let args = Array.prototype.slice.call(arguments, 1);
+	return fmtstr.replace(/\{(\d+)\}/g, function (match, index) {
+		return args[index];
+	});
 }
 
 //---------------------------------------------
-function number2Time(num)
-{
-    let hours    = Math.round(num/60);
-    let minutes  = Math.round(num%60);
-    let min_text = "minute";
-    let hr_text  = "hour";
-    let str;
+function number2Time(num) {
+	let hours = Math.round(num / 60);
+	let minutes = Math.round(num % 60);
+	let min_text = "minute";
+	let hr_text = "hour";
 
-    if (hours >= 2)
-        hr_text = "hours";
-    if (minutes >= 2)
-        min_text = "minutes";
+	if (hours >= 2) hr_text = "hours";
+	if (minutes >= 2) min_text = "minutes";
 
-    str = format('{0} {1}, {2} {3}', hours, hr_text, minutes, min_text);
-    console.log(str);
-
+	let str = format("{0} {1}, {2} {3}", hours, hr_text, minutes, min_text);
+	console.log(str);
 }
 //---------------------------------------------
 number2Time(133);
