2270c09aac641eacce55f85e543db9e65f7d7de5
Mabizza <william.paulsen@umuzi.org>
2022-03-08 12:35:51 +0200

    Prettied up a Task05 with Prettier

--- Task05.js
+++ Task05.js
@@ -1,10 +1,8 @@
-function triangleCalc(a, b, c)
-{
-   let side = (a + b + c)/2 ; 
-   triangle = (side * (side-a)*(side-b)*(side-c));
-   return (Math.sqrt( triangle ));
+function triangleCalc(a, b, c) {
+	let side = (a + b + c) / 2;
+	let triangle = side * (side - a) * (side - b) * (side - c);
+	return Math.sqrt(triangle);
 }
 
-let area = triangleCalc(4,7, 9);
+let area = triangleCalc(4, 7, 9);
 console.log(area.toFixed(2));
- 
