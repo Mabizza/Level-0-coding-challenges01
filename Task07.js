21aff8500860742f083e579111ad4cf99cea04ce
Mabizza <william.paulsen@umuzi.org>
2022-03-08 12:36:01 +0200

    Prettied up a Task07 with Prettier

--- Task07.js
+++ Task07.js
@@ -1,15 +1,13 @@
-function convertCelsiusToFahrenheit(degree)
-{
-    let result = Math.round(((9 * degree) / 5 + 32));
-    console.log(result);
+function convertCelsiusToFahrenheit(degree) {
+	let result = Math.round((9 * degree) / 5 + 32);
+	console.log(result);
 }
 
 //---------------------------------------------
-function convertFahrenheitToCelsius(degree)
-{
-    let result = Math.round((degree - 32) * 5 / 9);
-    console.log(result);
+function convertFahrenheitToCelsius(degree) {
+	let result = Math.round(((degree - 32) * 5) / 9);
+	console.log(result);
 }
-//---------------------------------------------
+
 convertCelsiusToFahrenheit(0);
 convertFahrenheitToCelsius(32);
