86a5dd89748bba5b3e17321819c84bb145a73d0a
Mabizza <william.paulsen@umuzi.org>
2022-03-10 07:02:55 +0200

    Changed variables to const type

--- Task05.js
+++ Task05.js
@@ -1,8 +1,8 @@
 function triangleCalc(a, b, c) {
-	let side = (a + b + c) / 2;
-	let triangle = side * (side - a) * (side - b) * (side - c);
+	const side = (a + b + c) / 2;
+	const triangle = side * (side - a) * (side - b) * (side - c);
 	return Math.sqrt(triangle);
 }
 
-let area = triangleCalc(4, 7, 9);
+let area = triangleCalc(4, 7, 8);
 console.log(area.toFixed(2));
