e620a2f2b6cc0f7f438ded93519fd3729380ef50
Mabizza <william.paulsen@umuzi.org>
2022-03-10 07:08:27 +0200

    camelCased variables.  Modified to return the maximum number not print it.

--- Task06.js
+++ Task06.js
@@ -1,11 +1,11 @@
 function maximum(...numbers) {
-	let maxno = numbers[0];
+	let maxNo = numbers[0];
 
 	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > maxno) maxno = numbers[i];
+		if (numbers[i] > maxNo) maxNo = numbers[i];
 	}
 
-	console.log(maxno);
+	return(maxNo);
 }
 
-maximum(15, 7, 10); // Task 0.6
+maximum(15, 7, 10);
