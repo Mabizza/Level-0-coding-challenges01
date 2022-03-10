3ea25d9e3663c810b0e1ab64852976c36ac6784f
Mabizza <william.paulsen@umuzi.org>
2022-03-10 07:12:37 +0200

    Return the output instead of printing it, declared variable result as a constant in the function.

--- Task07.js
+++ Task07.js
@@ -1,12 +1,12 @@
 function convertCelsiusToFahrenheit(degree) {
-	let result = Math.round((9 * degree) / 5 + 32);
-	console.log(result);
+	const result = Math.round((9 * degree) / 5 + 32);
+	return(result);
 }
 
 //---------------------------------------------
 function convertFahrenheitToCelsius(degree) {
-	let result = Math.round(((degree - 32) * 5) / 9);
-	console.log(result);
+	const result = Math.round(((degree - 32) * 5) / 9);
+	return(result);
 }
 
 convertCelsiusToFahrenheit(0);
